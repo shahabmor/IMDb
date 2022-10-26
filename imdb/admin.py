9cb01733a3a14c3b828bbf3dc40a82568d5fb7f4
Shahab Moradi <shahab.m.90@gmail.com>
2022-10-26 22:34:59 +0330

    update admin pannel

--- imdb/admin.py
+++ imdb/admin.py
@@ -4,3 +4,6 @@ from imdb.models import *
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Movie)
+admin.site.register(Crew)
+admin.site.register(MovieCrew)
+admin.site.register(Role)
