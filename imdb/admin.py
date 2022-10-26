7d43c908bd5f8e78bf67c62afbd40cb569affefe
Shahab Moradi <shahab.m.90@gmail.com>
2022-10-26 22:12:18 +0330

    Make movie model

--- imdb/admin.py
+++ imdb/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from imdb.models import *
 
 # Register your models here.
+admin.site.register(Genre)
--- imdb/models.py
+++ imdb/models.py
@@ -21,3 +21,12 @@ class Crew(models.Model):
     gender = models.PositiveSmallIntegerField(null=True, choices=GENDER_CHOICES)
     birthday = models.DateField(blank=True)
     avatar = models.ImageField(upload_to='actors/', null=True, blank=True)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    release_date = models.DateField(null=True)
+    avatar = models.ImageField(upload_to='movies/', null=True, blank=True)
+    genres = models.ManyToManyField(Genre)
+    crew = models.ManyToManyField(Crew)
