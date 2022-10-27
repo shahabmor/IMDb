ba0202702b0dc3d72187b1ddbe6215014a1c5a0a
Shahab Moradi <shahab.m.90@gmail.com>
2022-10-27 09:28:15 +0330

    Update crew model

--- imdb/models.py
+++ imdb/models.py
@@ -20,7 +20,7 @@ class Crew(models.Model):
     last_name = models.CharField(max_length=50)
     gender = models.PositiveSmallIntegerField(null=True, choices=GENDER_CHOICES)
     birthday = models.DateField(blank=True)
-    avatar = models.ImageField(upload_to='actors/', null=True, blank=True)
+    avatar = models.ImageField(upload_to='crew/', null=True, blank=True)
 
 
 class Movie(models.Model):
