b78d3fec409bf202272a189821499f29e05157a0
Sina Kamrani <kamrani.sina22@gmail.com>
2022-11-14 23:32:46 +0330

    edit form

--- movies/forms.py
+++ movies/forms.py
@@ -3,6 +3,11 @@ from .models import *
 
 
 class MovieForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(MovieForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = visible.field.label
 
     class Meta:
         model = Movie
